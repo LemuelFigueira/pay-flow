3b52809c59504b5a653bd192c991ca4c9d442169
Lemuel Figueira <pereiraleleo87@gmail.com>
2022-03-14 11:13:16 -0300

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { t } from '../i18n';
+</script>
+
+<main>
+	<h1>{$t('Hello')}</h1>
+</main>
+
+<style lang="scss">
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		width: 100%;
+		height: 100%;
+
+		gap: 1rem;
+
+		padding: 1rem;
+
+		background: var(--clr-light);
+	}
+</style>
