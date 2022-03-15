36977037f73033318ec18fa52fc9bf96fb47b235
Lemuel Figueira <lemuel.figueira@iksolution.com.br>
2022-03-14 21:55:09 -0300

    feat: signin page

---
+++ src/components/Button.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	// import Loading from './loading.svelte';
+	import BiErrorCircle from 'svelte-icons-pack/bi/BiErrorCircle';
+	import HiSolidXCircle from 'svelte-icons-pack/hi/HiSolidXCircle';
+	export let icon: string = '';
+	import Icon from 'svelte-icons-pack';
+	export let status: string = '';
+	$: if (status === 'error')
+		setTimeout(() => {
+			status = '';
+		}, 2000);
+</script>
+
+<button on:click>
+	{#if status === 'loading'}
+		<!-- <Loading /> -->
+		Loading..
+	{:else if status === 'error'}
+		<Icon className="icon" src={BiErrorCircle} />
+	{:else if icon === 'x'}
+		<Icon className="icon" src={HiSolidXCircle} />
+	{:else}
+		<slot />
+	{/if}
+</button>
+© 2022 GitHub, Inc. Terms
+
+<style type="text/scss">
+	button {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: 0.5rem;
+		font-family: 'Poppins', sans-serif;
+		border: 1px solid #ccc;
+		border-radius: var(--br);
+		outline: none;
+		color: var(--text);
+		background: var(--secondary);
+		&:hover {
+			filter: brightness(0.95);
+			cursor: pointer;
+			transition: ease-in-out 0.2s;
+		}
+		&:active {
+			border-color: var(--input-focus);
+			transform: scale(0.9);
+		}
+		:global {
+			.icon {
+				font-size: x-large;
+				fill: var(--input-error);
+				stroke: var(--input-error);
+			}
+		}
+	}
+</style>
---
+++ src/components/TextInput.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	export let value: string;
+	export let placeholder: string;
+	export let password: boolean = false;
+</script>
+
+{#if !password}
+	<input type="text" bind:value {placeholder} />
+{:else if password}
+	<input type="password" bind:value {placeholder} />
+{/if}
+
+<style type="text/scss">
+	input {
+		border: 2px solid var(--border-inactive);
+		padding: 5px;
+		outline: none !important;
+		border-radius: 5px;
+		font-weight: 600;
+		width: 100%;
+		background: var(--input-background);
+		filter: brightness(0.9);
+		&::placeholder {
+			font-size: small;
+			font-weight: 500;
+		}
+		&:hover {
+			/* border: 1px solid var(--input-hover); */
+			border-color: var(--input-hover);
+		}
+		&:focus {
+			/* border: 1px solid var(--input-focus); */
+			border-color: var(--input-focus);
+			filter: brightness(1);
+		}
+	}
+</style>
